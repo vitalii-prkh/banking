2b6f835ab97ef1590dfd81e25ec3ddff4005de6c
Vitalii Parkhomenko <vitalii.prkh+github@gmail.com>
2025-10-06 13:43:34 +0300

    Refactored `RecentTransactions` to use `ROUTES.TRANSACTION_HISTORY` constant for transaction history link.

--- components/RecentTransactions.tsx
+++ components/RecentTransactions.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import {ROUTES} from "@/constants/routes";
 import {Tabs, TabsContent, TabsList, TabsTrigger} from "@/components/ui/tabs";
 import {BankTabItem} from "./BankTabItem";
 import {BankInfo} from "./BankInfo";
@@ -27,7 +28,7 @@ export function RecentTransactions({
       <header className="flex items-center justify-between">
         <h2 className="recent-transactions-label">Recent transactions</h2>
         <Link
-          href={`/transaction-history/?id=${appwriteItemId}`}
+          href={`${ROUTES.TRANSACTION_HISTORY}/?id=${appwriteItemId}`}
           className="view-all-btn"
         >
           View all
